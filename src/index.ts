8da5415fa1e62facd29ef1e84badf5292cff93d8
WEDDEsign <anderswedde@gmail.com>
2026-02-28 16:56:42 +0100

    feat: add src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @mentra-intelligence/react
+ * React renderer for Mentra headless CMS content.
+ * @packageDocumentation
+ */
+export { MentraContent } from "./MentraContent";
+export type {
+  TipTapNode, TipTapMark, ImageAttrs,
+  NodeRenderer, MarkRenderer, MentraContentProps,
+} from "./types";
